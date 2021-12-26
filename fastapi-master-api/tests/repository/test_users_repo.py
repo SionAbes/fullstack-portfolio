de921c39102b5bcc5be0536155db8e264e2c25e5
sionabraham <sion.abraham@tacto.ai>
2021-12-26 18:05:11 +0200

    :white_check_mark: added passing tests for crud operations

--- fastapi-master-api/tests/repository/test_users_repo.py
+++ fastapi-master-api/tests/repository/test_users_repo.py
@@ -4,6 +4,57 @@ import pytest
 from app.domain.models.user import CreateUser
 
 
+def test_get(session, freezer):
+    user = UserFactory()
+    user_db = users_repo.get(
+        db=session,
+        id=user.id
+    )
+    assert user_db.first_name == user.first_name
+    assert user_db.last_name == user.last_name
+    assert user_db.id == user.id
+    assert user_db.created_at == user.created_at
+    assert user_db.is_superuser == user.is_superuser
+    assert user_db.email == user.email
+    assert user_db.last_login_at == user.last_login_at
+
+
+def test_get_no_user(session):
+    user_db = users_repo.get(
+        db=session,
+        id=-1
+    )
+    assert user_db == None
+
+
+def test_list(session, freezer):
+    UserFactory.create_batch(2)
+    user_db = users_repo.list(
+        db=session,
+    )
+    assert len(user_db) == 2
+
+
+def test_update(session):
+    user_update = CreateUser(
+        is_superuser=True,
+        first_name="_first_name",
+        last_name="_last_name",
+        email="_email",
+        password="_password",
+    )
+    user = UserFactory()
+    user_db = users_repo.update(
+        db=session,
+        id=user.id,
+        obj_in=user_update,
+    )
+    assert user_db.first_name == user.first_name
+    assert user_db.last_name == user.last_name
+    assert user_db.is_superuser == user.is_superuser
+    assert user_db.email == user.email
+
+
 def test_get_by_email(session, freezer):
     user = UserFactory()
     user_db = users_repo.get_by_email(
