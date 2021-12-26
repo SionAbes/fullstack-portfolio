b081e686e7efa449285fd0ae846b022560715be6
sionabraham <sion.abraham@tacto.ai>
2021-12-26 17:00:34 +0200

    :memo: added Snyk badge

--- fastapi-master-api/tests/repository/test_users_repo.py
+++ fastapi-master-api/tests/repository/test_users_repo.py
@@ -1,6 +1,8 @@
 from app.repository.database.users import users_repo
 from tests.factories.user import UserFactory
 import pytest
+from app.domain.models.user import CreateUser
+
 
 def test_get_by_email(session, freezer):
     user = UserFactory()
@@ -18,7 +20,7 @@ def test_get_by_email(session, freezer):
 
 
 def test_get_by_email_not_exists(session, freezer):
-    user = UserFactory()
+    UserFactory()
     user_db = users_repo.get_by_email(
         db=session,
         email="_email"
@@ -32,4 +34,30 @@ def test_get_by_email_no_session(freezer):
         users_repo.get_by_email(
             db=None,
             email=user.email
+        )
+
+
+def test_create(session):
+    user = CreateUser(
+        is_superuser=True,
+        first_name="_first_name",
+        last_name="_last_name",
+        email="_email",
+        password="_password",
+    )
+    user_db = users_repo.create(
+        db=session,
+        obj_in=user
+    )
+    assert user_db.first_name == user.first_name
+    assert user_db.last_name == user.last_name
+    assert user_db.is_superuser == user.is_superuser
+    assert user_db.email == user.email
+
+
+def test_create_no_user(session):
+    with pytest.raises(AttributeError):
+        users_repo.create(
+            db=session,
+            obj_in=None
         )
