ceac78beae22351ac805f7dbfe40ef77c9cbcd06
sionabraham <sion.abraham@tacto.ai>
2021-12-28 00:14:05 +0200

    :white_check_mark: added passing tests create_token

---
+++ fastapi-master-api/tests/domain/test_auth.py
@@ -0,0 +1,30 @@
+from app.domain.auth import create_token
+from datetime import timedelta, datetime, date
+import jwt
+from freezegun import freeze_time
+
+
+@freeze_time(datetime(2015, 5, 1, 15, 20, 30))
+def test_create_token(freezer, mock_admin_user):
+    token = create_token(
+        token_type="access_token",
+        lifetime=timedelta(minutes=10),
+        sub=mock_admin_user.sub,
+        roles=mock_admin_user.roles,
+    )
+    decoded_token = jwt.decode(token, "jwt_secret", algorithms=["HS256"])
+    assert decoded_token["roles"] == ['ADMIN', 'USER']
+    assert int(decoded_token["sub"]) == mock_admin_user.sub
+    assert decoded_token["type"] == mock_admin_user.type
+    assert datetime.fromtimestamp(decoded_token["iat"]).year == datetime.now().year
+    assert datetime.fromtimestamp(decoded_token["iat"]).month == datetime.now().month
+    assert datetime.fromtimestamp(decoded_token["iat"]).day == datetime.now().day
+    assert datetime.fromtimestamp(decoded_token["iat"]).hour == datetime.now().hour
+    assert datetime.fromtimestamp(decoded_token["iat"]).minute == datetime.now().minute
+    assert datetime.fromtimestamp(decoded_token["iat"]).second == datetime.now().second
+    assert datetime.fromtimestamp(decoded_token["exp"]).year == datetime.now().year
+    assert datetime.fromtimestamp(decoded_token["exp"]).month == datetime.now().month
+    assert datetime.fromtimestamp(decoded_token["exp"]).day == datetime.now().day
+    assert datetime.fromtimestamp(decoded_token["exp"]).hour == datetime.now().hour
+    assert datetime.fromtimestamp(decoded_token["exp"]).minute == (datetime.now() + timedelta(minutes=10)).minute
+    assert datetime.fromtimestamp(decoded_token["exp"]).second == datetime.now().second
