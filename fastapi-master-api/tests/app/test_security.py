49299f47d6e8b1f284fa44a36c23cdbd80bab609
sionabraham <sion.abraham@tacto.ai>
2021-12-27 23:49:08 +0200

    :test_tube: added failing test for verify_password

---
+++ fastapi-master-api/tests/app/test_security.py
@@ -0,0 +1,16 @@
+import pytest
+import passlib
+from app.security import verify_password
+
+
+def test_verify_password():
+    password = "test"
+    hashed_password = "$2b$12$Dhp7oFAnpFeyLmA/tLAzhefy/ipC641qZg.3RNW0VvYmbqiQmHMLG"
+    assert verify_password(plain_password=password, hashed_password=hashed_password) is True
+
+
+def test_verify_password_false():
+    password = "test"
+    hashed_password = "_hashed_password_false"
+    with pytest.raises(passlib.exc.UnknownHashError):
+        verify_password(plain_password=password, hashed_password=hashed_password)
