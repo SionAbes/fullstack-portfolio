9864d26c5e1a4d188cb7235ed95af99036348a2f
sionabraham <sion.abraham@tacto.ai>
2021-12-28 13:16:04 +0200

    :memo: generate badges for users put delete

---
+++ fastapi-master-api/generate_reports.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd fastapi-master-api
+pytest --junitxml=reports/junit/junit.xml --html=reports/junit/report.html
+poetry run coverage xml -o ./reports/coverage/coverage.xml --omit="*/test*"
+poetry run coverage html -d ./reports/coverage --omit="*/test*"
+poetry run flake8 --exit-zero --format=html --htmldir ./reports/flake8 --statistics --tee --output-file ./reports/flake8/flake8stats.txt
+genbadge tests -o ./reports/badges/tests-badge.svg
+genbadge coverage -o ./reports/badges/coverage-badge.svg
+genbadge flake8 -o ./reports/badges/flake8-badge.svg
