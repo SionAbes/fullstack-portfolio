a59c420326977504de40bd6f4edc3a136fb1659d
sionabraham <sion.abraham@tacto.ai>
2022-01-02 16:21:46 +0200

    :memo: Updated Badges

--- fastapi-master-api/app/repository/models/__init__.py
+++ fastapi-master-api/app/repository/models/__init__.py
@@ -1,2 +1,2 @@
-from app.repository.models.adapters import Adapter
-from app.repository.models.users import User
+from app.repository.models.adapters import Adapter  # noqa: F401
+from app.repository.models.users import User  # noqa: F401
