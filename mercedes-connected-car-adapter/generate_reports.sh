08d444b88667a3bf1219516ff419935934ef6480
sionabraham <sion.abraham@tacto.ai>
2022-01-31 21:08:43 +0100

    :fire: burned mercedes. They deprecated this API this month :(

--- mercedes-connected-car-adapter/generate_reports.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd mercedes-connected-car-adapter
-pytest --cov=./src --cov-report html:./reports/coverage --cov-report xml:./reports/coverage/coverage.xml --junitxml=reports/junit/junit.xml --html=reports/junit/report.html
-poetry run flake8 --exclude ./alembic --exit-zero --format=html --htmldir ./reports/flake8 --statistics --tee --output-file ./reports/flake8/flake8stats.txt
-genbadge tests -o ./reports/badges/tests-badge.svg
-genbadge coverage -o ./reports/badges/coverage-badge.svg
-genbadge flake8 -o ./reports/badges/flake8-badge.svg
--- mercedes-connected-car-adapter/src/models/shared.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class RetrievalStatus(Enum):
-    VALID = "VALID"
-    INITIALIZED = "INITIALIZED"
-    INVALID = "INVALID"
-    NOT_SUPPORTED = "NOT_SUPPORTED"
